a6bf945a84cc3feb290d5973313dd88bedeba9ac
Nick Nguyen <nicknguyen@b98-aruba-authenticated-10-110-161-132.wlan.cpp.edu>
2024-02-27 16:12:11 -0800

    Initial Commit

---
+++ bonus_assignment.py
@@ -0,0 +1,18 @@
+# Authors: Benjamin Nguyen, Nick Nguyen
+# Assignment:  Bonus Assignment
+# Completed (or last revision): 02/29/2024
+
+# Design Idea Statement
+"""
+We were inspired by ...
+
+Team members: Benjamin Nguyen, Nick Nguyen
+Benjamin Nguyen contribution: coded the source code
+Nick Nguyen contribution: coded the source code
+"""
+# Source Code
+
+from PIL import Image
+
+myImage = Image.open('test.jpeg')
+myImage.show()
